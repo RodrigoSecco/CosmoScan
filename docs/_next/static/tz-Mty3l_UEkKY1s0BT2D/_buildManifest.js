d40b8afd631a2b5109fa93436fe6bd864e42e8ea
Rodrigo <rodrigo.secco@cliqx.com.br>
2025-09-03 19:57:04 -0300

    docs: publish static export for GitHub Pages

---
+++ docs/_next/static/tz-Mty3l_UEkKY1s0BT2D/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/1d2155fdb01ee7ee.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/CosmoScan//_next/:path+",
+        "destination": "/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
